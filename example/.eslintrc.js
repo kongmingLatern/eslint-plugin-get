10d7a4bb9b489143f69fea5b0cc6c777637835bf
凤之兮原 <1577310596@qq.com>
2022-11-15 20:42:25 +0800

    feat: add playground and install plugin

---
+++ example/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins:": [
+        "get"
+    ],
+    "rules": {
+        "get/get": ["warn"]
+    }
+}
